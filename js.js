06cc3a46acb84b3451b61abbb899602530af2e09
cirogoud <45541376+cirogoud@users.noreply.github.com>
2020-01-27 02:09:02 +0100

    Add files via upload

---
+++ js.js
@@ -0,0 +1,8 @@
+function toonMeer() {
+    var x = document.getElementById("showMore");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
